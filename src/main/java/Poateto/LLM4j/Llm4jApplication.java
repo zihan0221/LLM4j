924f5f59400c58915036e692d4978e4fbfbda35c
Ateto1202 <tonysu1204@gmail.com>
2024-04-15 00:44:32 +0800

    Initial commit

---
+++ src/main/java/Poateto/LLM4j/Llm4jApplication.java
@@ -0,0 +1,13 @@
+package Poateto.LLM4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Llm4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Llm4jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Poateto/LLM4j/Llm4jApplicationTests.java
@@ -0,0 +1,13 @@
+package Poateto.LLM4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Llm4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
